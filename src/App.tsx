361b7c2027894232fb23fe5f235bd97d8c8f00ea
Felipeserpa <felipe.mago_skt@hotmail.com>
2024-01-25 23:54:44 -0300

    atualizaçao  da paginas

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,8 @@
 import { createBrowserRouter } from "react-router-dom";
 
-import  Home  from "./page/Home"
-import  Cadastro  from './page/Cadastro'
-import   Login  from './page/Login'
+import  Home from "./page/home"
+import  Cadastro  from './page/cadastro'
+import   Login  from './page/login'
 
 const router =createBrowserRouter([
   {
--- src/page/Home/index.tsx
+++ src/page/Home/index.tsx
@@ -1,10 +1,19 @@
 
+export default function Home() {
+    return (
 
-export default function Home(){
-    return(
+        <div className='grid justify-items-center mt-24'>
+            <img src="image/logo.jpg" alt="barbearia" style={{ width: 200 }} />
+            <p className="">Sejam bem vindo</p>
+            <div>
+                <nav className="flex justify-center space-x-4">
+                   
+                    <a href="/login" className="font-medium px-3 py-2 text-slate-700 rounded-lg hover:bg-slate-100 hover:text-slate-900">Login</a>
+                    <a href="/Cadastro" className="font-medium px-3 py-2 text-slate-700 rounded-lg hover:bg-slate-100 hover:text-slate-900">Cadastro</a>
 
-        <div>
-            <p>Home</p>
+                </nav>
+            </div>
+
         </div>
 
     )
