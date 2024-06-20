8a0a719cd62f1f30751189e4eb42869724c1887d
Felipeserpa <felipe.mago_skt@hotmail.com>
2024-06-20 10:53:39 -0300

    alteração no footer da pagina cliente

--- src/page/cliente/index.tsx
+++ src/page/cliente/index.tsx
@@ -1,41 +1,40 @@
+import Modal from "../../components/modal";
 
-import Modal from '../../components/modal';
+import Navbar from "../../components/navbar";
 
-import Navbar from '../../components/navbar';
-
-import Footer from '../../components/footer';
+import Footer from "../../components/footer";
 
 const ClientPage = () => {
-
   return (
-
-
     <div>
       <Navbar />
 
       <div className=" text-white">
-
         <div className="flex flex-row mr-60 ">
           <div className="basis-1/4 ml-20 mx-8 mt-1">
-            <img src="image/foto01.jpg" className='rounded-full  mt-3 ' alt="barbearia" style={{ width: 156 }} />
+            <img
+              src="image/foto01.jpg"
+              className="rounded-full  mt-3 "
+              alt="barbearia"
+              style={{ width: 156 }}
+            />
             <Modal></Modal>
           </div>
           <div className="basis-1/4">
-            <img src="image/foto02.jpg" className='rounded-full mt-3 ' alt="barbearia " style={{ width: 152 }} />
+            <img
+              src="image/foto02.jpg"
+              className="rounded-full mt-3 "
+              alt="barbearia "
+              style={{ width: 152 }}
+            />
             <Modal></Modal>
           </div>
         </div>
-        <div className='mt-28'>
-              <Footer/>
+        <div className="mt-44">
+          <Footer />
         </div>
-    
       </div>
-
     </div>
-
   );
-
-
 };
 export default ClientPage;
-
