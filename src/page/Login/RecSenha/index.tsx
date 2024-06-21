0805750da956d30b281dc299c3eb38f756da07af
Felipe serpa <felipe.mago_skt@hotmail.com>
2024-06-21 16:01:18 -0300

    atualizacao da Lp Recupera senha

--- src/page/Login/RecSenha/index.tsx
+++ src/page/Login/RecSenha/index.tsx
@@ -1,11 +1,11 @@
 import React from "react";
 
 import { useState } from "react";
-import { Link } from "react-router-dom";
+
 import logoBarb from "./../../../../public/images/logo.jpg";
 export default function recSenha() {
   const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
+
   //const navigate = useNavigate();
 
   return (
@@ -33,35 +33,12 @@ export default function recSenha() {
             />
             <p className="text-slate-300">{Error.email?.message}</p>
 
-            <label className="p-1 text-slate-300">Senha:</label>
-
-            <input
-              type="password"
-              id="password"
-              className="rounded pl-1"
-              name="password"
-              placeholder="*****"
-              value={password}
-              onChange={(e) => setPassword(e.target.value)}
-            />
-            <p className="text-slate-300">{Error.password?.message}</p>
-
             <button
               type="submit"
-              className="h-9 mt-3  bg-blue-600 rounded border-1 text-lg font-medium text-white p-1 "
+              className="h-9 mt-3  bg-red-600 rounded border-1 text-lg font-medium text-white p-1 "
             >
-              Login
+              Redefinir
             </button>
-            <Link to="/cadastro">
-              <p className="font-medium px-3 py-2 text-slate-100  hover:text-slate-500">
-                Não tem conta?Cadastre-se
-              </p>
-            </Link>
-
-            <Link to="/login/RecSenha">
-              {" "}
-              <p className="text-slate-300">Esqueceu a senha?</p>
-            </Link>
           </form>
         </div>
       </div>
