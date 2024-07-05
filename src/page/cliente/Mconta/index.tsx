509a34114d3ef965b2b1f9964e2c2a6d17ee9492
Felipe serpa <felipe.mago_skt@hotmail.com>
2024-07-05 11:13:41 -0300

    atualizando sidebar

--- src/page/cliente/Mconta/index.tsx
+++ src/page/cliente/Mconta/index.tsx
@@ -1,44 +1,102 @@
 import { SetStateAction, useState } from "react";
 import Navbar from "../../../components/navbar";
+
+import logoBarb from "./../../../../public/images/logo.jpg";
+
 export default function Mconta() {
   const [currentPage, setCurrentPage] = useState("page-1");
+  const [nome, setNome] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
 
   const handleClick = (nextPage: SetStateAction<string>) => {
     setCurrentPage(nextPage); // Update state on link click
   };
+
   return (
     <div>
       <Navbar />
 
-      <div className="flex h-screen">
-        <div className="w-1/6 ">
+      <div className="flex h-screen ">
+        <div className="w-1/14  sm:w-1/4 md:w-1/6">
           {/* Conteúdo da coluna aqui */}
           <nav className=" flex-1 flex flex-col text-white">
             <a
-              className="nav-link flex p-3 hover:bg-gray-700 text-white"
+              className="nav-link flex p-3 hover:bg-gray-700 text-white  mb-2 text-base"
               onClick={() => handleClick("page-1")}
             >
-              <span className="text-sm">Home</span>
+              <span className="text-sm">Perfil do Usuário</span>
             </a>
             <a
               href="#"
-              className="nav-link flex p-3 hover:bg-gray-700"
+              className="nav-link flex p-3 hover:bg-gray-700  mb-2 text-base"
               onClick={() => handleClick("page-2")}
             >
-              <span className="text-sm">Dashboard</span>
+              <span className="text-sm">Histórico de Compras</span>
             </a>
-            <a href="#" className="nav-link flex p-3 hover:bg-gray-700">
-              <span className="text-sm">Settings</span>
+            <a
+              href="#"
+              className="nav-link flex p-3 hover:bg-gray-700  mb-2 text-base"
+            >
+              <span className="text-sm">Agendamentos e Reservas</span>
             </a>
           </nav>
         </div>
 
-        <div className="content flex-grow p-4 text-center bg-white">
+        <div className="content flex-grow p-4  bg-white">
           <div
             id="pagina-1"
             className={currentPage === "page-1" ? "" : "hidden"}
           >
-            <h1>Conteúdo da Página 1</h1>
+            <div>
+              <div className="grid justify-items-center mt-24">
+                <img
+                  src={logoBarb}
+                  className="rounded-full"
+                  alt="barbearia"
+                  style={{ width: 200 }}
+                />
+                <form className="w-full max-w-xl flex flex-col  p-2">
+                  <label className="p-1">Nome:</label>
+
+                  <input
+                    type="text"
+                    className="rounded pl-1"
+                    placeholder="Digite seu nome"
+                    name="nome"
+                    value={nome}
+                    onChange={(e) => setNome(e.target.value)}
+                  />
+
+                  <label className="p-1">Email:</label>
+
+                  <input
+                    type="email"
+                    className="rounded pl-1"
+                    placeholder="Digite seu email"
+                    name="email"
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value)}
+                  />
+
+                  <label className="p-1  ">Senha:</label>
+
+                  <input
+                    type="password"
+                    className="rounded pl-1"
+                    placeholder="Digite sua senha"
+                    name="password"
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}
+                  />
+                  <p className="text-slate-300"></p>
+
+                  <button className="h-9 mt-3  bg-blue-600 rounded border-1 text-lg font-medium text-white p-1">
+                    Salvar Alterações
+                  </button>
+                </form>
+              </div>
+            </div>
             {/* Add your page 1 content here */}
           </div>
 
