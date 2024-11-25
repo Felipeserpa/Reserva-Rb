4e02eab639de7722fbed6594ff952fa298f6f749
Felipe serpa <felipe.mago_skt@hotmail.com>
2024-11-25 11:10:02 -0300

    atualizacao do sistema completo

--- src/components/calendarios/index.tsx
+++
@@ -1,7 +0,0 @@
-import React, { useState } from "react";
-import { db } from "../../services/firebaseConection";
-import { collection, getDocs, query, where } from "firebase/firestore";
-
-const Calendario = () => {};
-
-export default Calendario;
