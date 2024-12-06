162ce3486e22e627127ceee08296c297b7804373
Felipe serpa <felipe.mago_skt@hotmail.com>
2024-12-06 20:46:19 -0300

    atualizando a pagina cliente para agendamento

--- src/components/footer/index.tsx
+++ src/components/footer/index.tsx
@@ -1,52 +1,49 @@
-
 import { SlSocialInstagram } from "react-icons/sl";
 import { ImWhatsapp } from "react-icons/im";
 
 import { Link } from "react-router-dom";
-
-
-
+import React from "react";
 
 const footer = () => {
-
-
   return (
-
-    <div className="bg-slate-900 pt-2  pb-6 pr-20">
-      <div className="h-38 grid grid-cols-3 gap-4 content-center">
-
-        <div className="ml-8 text-white">
-          <p className='font-mono'>Reserva Barbearia</p>
-          <p>Contato: (81) 99807-8137 </p>
+    <div className="bg-slate-900 pt-6 pb-8 px-4 sm:px-8 lg:px-20 flex justify-center">
+      <div className="w-full max-w-4xl grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8 items-center text-center">
+        {/* Informações de Contato */}
+        <div className="text-white">
+          <p className="font-mono text-lg">Reserva Barbearia</p>
+          <p>Contato: (81) 99807-8137</p>
         </div>
 
-        <div className="ml-12 text-white">
-          <p>Horario de Atendimento:</p>
-          <p>Segunda a Sexta 09:00 Ás 21:00</p>
-          <p>Sabado 09:00 Ás 21:00</p>
-          <p>Domingo 09:00 Ás 13:00</p>
-
+        {/* Horário de Atendimento */}
+        <div className="text-white">
+          <p className="font-semibold">Horário de Atendimento:</p>
+          <p>Segunda a Sexta 09:00 às 21:00</p>
+          <p>Sábado 09:00 às 21:00</p>
+          <p>Domingo 09:00 às 13:00</p>
         </div>
 
-        <div className="ml-16  text-white ">
-          <p>Rede socias</p>
-          <div className=" size-6 pt-1">
-            <Link to='https://www.instagram.com/reserva_barbearia/'>
+        {/* Redes Sociais */}
+        <div className="text-white">
+          <p className="font-semibold">Redes Sociais</p>
+          <div className="flex justify-center gap-4 mt-2">
+            <Link
+              to="https://www.instagram.com/reserva_barbearia/"
+              className="text-xl"
+            >
               <SlSocialInstagram />
             </Link>
+            <Link
+              to="https://api.whatsapp.com/send?phone=5581998078137"
+              className="text-xl"
+            >
+              <ImWhatsapp />
+            </Link>
           </div>
-          <Link to='https://api.whatsapp.com/send?phone=5581998078137' className=""> <ImWhatsapp /></Link>
-
-          <p className="">º2024/Stecnologic</p>
+          <p className="mt-4 text-sm">º2024/Stecnologic</p>
         </div>
-
       </div>
-
     </div>
   );
-}
+};
 
-export default footer
-
-
-
+export default footer;
